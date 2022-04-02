36e57d48278a2d79744890de76ff3cb9a5e82a00
Shargorodskiy Vladimir <gsmkibernetik@gmail.com>
2022-04-02 14:15:16 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from pygame import *
+from random import randint
+font.init()
+
+# нам нужны такие картинки:
+img_back = "galaxy.jpg"  # фон игры
+img_hero = "rocket.png"  # герой
+img_enemy = "ufo.png"  # враг
+img_bullet = "bullet.png"
+
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+    # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    # метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx - 7.5 , self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+
+# класс спрайта-врага
+class Enemy(GameSprite):
+    # движение врага
+    def update(self):
+        global lost
+        self.rect.y += self.speed
+        # исчезает, если дойдет до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost += 1
+
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
